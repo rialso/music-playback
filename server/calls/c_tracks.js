b77449bec925dcd8c2c7d0fca66d7dda8b7c1aaa
rtb <rtb@rtb.local>
2016-07-26 13:22:01 +0200

    no message

--- server/calls/c_tracks.js
+++ server/calls/c_tracks.js
@@ -9,6 +9,7 @@ exports.c_TrackById = function( id ){
 		var doc = {
 
 			Location : "../music/The.madpix.project_-_Wish_You_Were_Here.mp3"
+			//Location : "../music/3_Love_Me_Two_Times.flac"
 		}
 
 
--- server/handlers/music-playback-convert-1.js
+++ server/handlers/music-playback-convert-1.js
@@ -43,7 +43,7 @@ exports.startPlayback = function(req, res) {
 
             //startTrackStreaming( req, res, path );
 
-            startTrackStreaming5( req, res, path );
+            startTrackStreaming3_6( req, res, path );
 
         } else {
             console.error('Could not find file ' + path);
@@ -279,13 +279,13 @@ var startTrackStreaming3_5 = function(req, res, path) {
 
         var child_process = require("child_process");
 
-        var ffmpeg = child_process.spawn("./ffmpeg", [
+        var ffmpeg = child_process.spawn("./ffmpeg", [ ///usr/local/bin/ffmpeg
             //'-re',
             //'-y',
             '-i', 'pipe:0', // path
             '-ss', '0',                 //starting time offset
             //"-bufsize", "64k",
-            '-f', 'ogg', // File format
+            '-f', 'wav', // File format
             'pipe:1' // Output to STDOUT
         ]);
         stream
@@ -311,6 +311,106 @@ var startTrackStreaming3_5 = function(req, res, path) {
             //'Accept-Ranges': 'bytes',
             //'Content-Range': 'bytes ',
             'Content-Type': 'audio/mpeg',
+        }); 
+
+        res.on('close', function() {
+            ffmpeg.kill();
+        });
+
+        //ffmpeg.stdout.pipe(res);
+};
+
+//http://superuser.com/questions/554962/converting-mp3-to-aac-and-outputting-to-stdout
+/*
+
+When you run this command:
+
+ffmpeg.exe -i s.mp3 o.aac
+you may notice the following line in the output:
+
+Output #0, adts, to 'o.aac':
+Here, ffmpeg is telling you that it is using the format called adts to generate an .aac file. Use -f adts instead of -f aac:
+
+ffmpeg.exe -i s.mp3 -f adts -
+shareimprove this answer
+
+*/
+
+
+var startTrackStreaming3_6 = function(req, res, path) {
+
+        //path = '/Users/rtb/Music/___nevera/Eilen Jewell/2015 Sundown Over Ghost Town/02 Hallelujah Band.mp3'
+        //path = 'input.mp3';
+        //path = 'output.ogg';
+
+        var stream = fs.createReadStream(path);
+
+        console.log( 'path: ', path +' | '+ req.headers.range )
+
+
+        var child_process = require("child_process");
+
+        var ffmpeg = child_process.spawn('./ffmpeg', [ ///usr/local/bin/ffmpeg   // "./ffmpeg"
+            //'-re',
+            //'-y',
+            //'-i', 'pipe:0', // path
+            
+            //"-b:v" , "64k",         // bitrate to 64k
+            //'-ss', '0',                 //starting time offset
+            //"-bufsize", "64k",
+            //'-f', 'wav', // File format
+            //'-f', 'aac', 
+            //'pipe:1' // Output to STDOUT
+            //"-" 
+
+
+            // '-i', path,    // Output to STDOUT
+            // '-f', 's16le', // PCM 16bits, little-endian
+            // '-ar', '44100', // Sampling rate
+            // '-ac', 2, // Stereo
+            // 'pipe:1' // Output to STDOUT
+
+            //'-i', path,    // Output to STDOUT
+            //'-acodec', 'libfdk_aac',
+            //'-f', 'mp4', // PCM 16bits, little-endian
+            //'-ar', '44100', // Sampling rate
+            //'-ac', 2, // Stereo
+            //'-f', 'wav', // File format
+
+            //'-ac', '2',
+            //'-c:a', '-acodec',
+            //'-b:a', '128k',
+            //'pipe:1' // Output to STDOUT
+
+
+            //'-i', 'pipe:0', // path
+            '-i', path,    // Output to STDOUT
+            '-f', 'adts',
+            //'-strict', '-2',
+            'pipe:1'
+        ]);
+        // stream
+        //     .on('data', function(chunk) {
+        //         //console.log(chunk)
+        //     })
+        //     .on('end', function() {
+        //         console.log(':::::::::finished...');
+        //     });
+
+        // stream.pipe(ffmpeg.stdin);
+
+        ffmpeg.stdout.pipe(res);
+
+        ffmpeg.stderr.on('data', function(data) { 
+
+            console.log('ffmpeg -- stderr: ' + data.toString());
+        }) 
+
+        res.writeHead(200, {
+            //'Accept-Ranges': 'bytes',
+            //'Content-Range': 'bytes ',
+            'Content-Type': 'audio/mpeg',
+            //'Content-Type': 'video/mp4',
         }); 
 
         res.on('close', function() {
@@ -359,20 +459,12 @@ var startTrackStreaming4 = function(req, res, path) {
         });
         ffmpeg.stdout.pipe(res);
 
-
-
         stream.pipe(ffmpeg.stdin);
-
-
-        
-
-
-
 
         res.writeHead(200, {
             //'Accept-Ranges': 'bytes',
             //'Content-Range': 'bytes ',
-            'Content-Type': 'audio/mpeg',
+            'Content-Type': 'audio/wav',
         }); 
 
         //ffmpeg.stdout.pipe(res);
@@ -395,7 +487,7 @@ function spawnFfmpeg2(exitCallback) {
             //"ultrafast",
             // '-c',
             // 'copy',
-            '-f', 'ogg', // File format
+            '-f', 'wav', // File format
             'pipe:1' // Output to STDOUT
         ];
                                             
@@ -420,7 +512,7 @@ var startTrackStreaming5 = function(req, res, path) {
         //path = 'input.mp3';
         //path = 'output.ogg';
 
-        var stream = fs.createReadStream(path);
+        //var stream = fs.createReadStream(path);
 
         console.log( 'path: ', path +' | '+ req.headers.range )
 
@@ -451,6 +543,72 @@ var startTrackStreaming5 = function(req, res, path) {
            console.log('grep stderr: ' +d.toString());
         });
 
+};
+
+var startTrackStreaming6 = function(req, res, path) {
+
+    var childProcess = require("child_process");
+
+    console.log( 'path: ', path +' | '+ req.headers.range )
+
+    //if(typeof request.headers.range !== 'undefined')
+    if (!!req.headers.range)  {
+        var range       = req.headers.range;
+        var ranges      = range.replace(/bytes=/, "").split("-");
+        var rangeStart  = ranges[0];
+        var rangeEnd    = ranges[1];
+        var total       = fs.statSync(path).size;
+        
+        var start = parseInt(rangeStart, 10);
+        var end   = rangeEnd ? parseInt(rangeEnd, 10) : total - 1;
+
+        res.writeHead(206, {
+            'Accept-Ranges': 'bytes',
+            'Content-Range': 'bytes ' + start + '-' + end + '/' + total,
+            'Content-Length': (end - start) + 1,
+            'Content-Type': 'audio/mpeg',
+            //'Connection': 'close'
+          });
+        
+        //var file = fs.createReadStream(path, {start: start, end: end});
+
+        var audioConv = childProcess.spawn('./audioConv.sh',[
+            path, {start: start, end: end},
+            0,                //second parameter specifies a start time offset
+            3,                //third parameter specifies which variable encoding to use 0-9, with 0 being the best 9 the worst
+        ]);
+
+    } else {
+
+        // un 200 causa crask in Firefox
+        res.writeHead(206, {
+            //'Content-Length': end,
+            'Content-Type': 'audio/mpeg',
+            //'Connection': 'close'
+          });
+
+        //var file = fs.createReadStream(path);
+
+        var audioConv = childProcess.spawn('./audioConv.sh',[
+            path,
+            0,                //second parameter specifies a start time offset
+            3,                //third parameter specifies which variable encoding to use 0-9, with 0 being the best 9 the worst
+        ]);
+        //audioConv.stdout.pipe(res);
+    }
+
+    //res.end();
+
+    audioConv.stdout.pipe(res);
+
+    res.on('close', function() {
+      audioConv.kill();
+    });
+
+    // file.pipe(res);
+    // file.on('close', function () {
+    //     res.end(0);
+    // });
 };
 
 /*
